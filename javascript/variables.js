241d44a36c03587780451aa6aaea4ab71791e8d1
Sonali Sharma <go.sonali.sharma@gmail.com>
2023-05-26 11:21:53 -0300

    added Math functions

---
+++ javascript/variables.js
@@ -0,0 +1,8 @@
+let address_line_1 = "Dr Jose Scoseria 2910"
+let address_line_2 = "Apt no 607"
+let city = "Pocitos"
+let state = "Montevideo"
+let zipcode = 11300
+let country = "Uruguay"
+
+let is_primary_address = true
