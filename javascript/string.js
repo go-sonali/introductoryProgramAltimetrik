50baef82663b1d9c0fb1e9a0a4d6011c643d6b43
Sonali Sharma <go.sonali.sharma@gmail.com>
2023-05-26 16:51:33 -0300

    learning strings

---
+++ javascript/string.js
@@ -0,0 +1,9 @@
+let game = "basket"
+console.log(game.length)
+console.log(game[1])
+console.log(game[0])
+console.log(game[3])
+console.log(game[game.length - 1])
+console.log(game.charAt(0))
+console.log(game.split('o'))
+console.log(game.concat('ball'))
